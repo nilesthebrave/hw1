2b92862208a085996ae34739e5895cf2f8cd43ec
nilesthebrave <nilesthebrave@gmail.com>
2017-12-06 13:33:38 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,35 @@
+/*  Glenn Niles
+    Homework 1
+    StraighterLine C++
+    12-6-2017    
+*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    // Draw triangle
+    cout << "   *   " << endl;
+    cout << "  ***  " << endl;
+    cout << " ***** " << endl;
+    cout << "*******" << endl;
+    cout << "  ***  " << endl;
+    
+    // Draw a cat 
+    cout << "    \n\n   " << endl;
+    cout << " /\\   /\\ " << endl;
+    cout << "   o o     " << endl;
+    cout << "  =   =    " << endl;
+    cout << "   ---     " << endl;
+    
+    // Draw Truck
+    cout << "             \n\n                   " << endl;
+    cout << "_________________ ||                " << endl;
+    cout << "|^^^^^^^^^^^^^^^^\\||____           " << endl;
+    cout << "|   SuperCool    |||''|'\\__,__     " << endl;
+    cout << "|______________  |||__|__|__|)|     " << endl;
+    cout << "(@)@)''''''''''**|(@)(@)**|(@)      " << endl;
+    
+    return 0;
+}
